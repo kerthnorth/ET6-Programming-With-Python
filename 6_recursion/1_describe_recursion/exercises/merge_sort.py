18439d9d3439522ae48c2f27dbfe0fbca83283a9
kerthnorth <isaackibwe646@gmail.com>
2024-12-20 22:16:37 +0200

    moving on to the next part of tyhe exercise

--- 6_recursion/1_describe_recursion/exercises/merge_sort.py
+++ 6_recursion/1_describe_recursion/exercises/merge_sort.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-""" Use test cases, the docstring, and labels to describe this solution. """
+""" Use test cases, the docstring, and labels to describe this solution. 
+The following code attempts to sort a list of numbers using the merge sort algorithm.
+we have a base case and a recursive case
+
+
+"""
 
 
 def merge_sort(numbers: list) -> list:
@@ -13,13 +18,13 @@ def merge_sort(numbers: list) -> list:
     recursive case:
 
     """
-    if len(numbers) <= 1:  #
-        return numbers  #
+    if len(numbers) <= 1:  # This is the base case
+        return numbers  # This part returns the base case
 
     mid = len(numbers) // 2
-    # 
+    # This is a part of the code that handles recursive case. it sorts our program from left to right
     left_half = merge_sort(numbers[:mid])
-    # 
+    # We then merge the sorted halves
     right_half = merge_sort(numbers[mid:])
 
     return merge(left_half, right_half)
@@ -38,3 +43,7 @@ def merge(left, right):
 
 
 # --- --- --- test cases --- --- ---
+print(merge_sort([]), "should be", [])
+print(merge_sort([2, 1]), "should be", [1, 2])
+print(merge_sort([1, 2, 3]), "should be", [1, 2, 3])
+print(merge_sort([5, 3, 8, 6, 2]), "should be", [2, 3, 5, 6, 8])

7104ebc5de4fe4fdba98861700b681985a7c2d7e
kerthnorth <isaackibwe646@gmail.com>
2024-12-24 18:38:52 +0200

    testing counting items in list

--- 6_recursion/3_use_recursion/2_write/count_items_in_list.py
+++ 6_recursion/3_use_recursion/2_write/count_items_in_list.py
@@ -1,5 +1,19 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-def count_items_in_list():
-    """"""
+def count_items_in_list(list_of_things):
+    """
+    Count the number of items in a list.
+
+    Args:
+        list_of_things (list): the list to count
+
+    Returns:
+        int: the number of items in the list
+    """
+    count = 0
+    for item in list_of_things:
+        count += 1
+    # print(count)
+    return count
+count_items_in_list([1, 2, 3])
